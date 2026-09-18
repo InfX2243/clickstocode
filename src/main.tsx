18db485160364f2ddc8bd1a08d52fd67863f8211
InfX2243 <abid.231446.it@mhssce.ac.in>
2026-09-18 08:39:53 +0530

    chore: load cinematic motion choreography

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import Home from './pages/Home';
 import './styles.css';
 import './cinematic-scroll.css';
+import './cinematic-motion.css';
 
 function App() {
   return (
