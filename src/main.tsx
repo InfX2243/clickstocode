5e343da54af75b97b8cc14479ddc542ffc0e41e7
InfX2243 <abid.231446.it@mhssce.ac.in>
2026-09-20 12:51:06 +0530

    Revert broken Event Day Timeline stylesheet import

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,5 @@ import './styles.css';
 import './cinematic-scroll.css';
 import './cinematic-motion.css';
 import './screen15-fix.css';
-import './event-day-timeline.css';
 
 createRoot(document.getElementById('root')!).render(<Home />);
